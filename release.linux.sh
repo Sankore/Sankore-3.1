f2f4268f4721ac300d38c031f21d49ac28294093
Claudio Valerio <claudio@sente.ch>
2011-08-31 18:32:26 +0200

    added code for tag

--- release.linux.sh
+++ release.linux.sh
@@ -37,18 +37,18 @@ $QMAKE_PATH -spec linux-g++
 
 make -j 4 release-install
 
-#VERSION=`cat build/linux/release/version`
-#if [ ! -f build/linux/release/version ]; then
-#    echo "version not found"
-#    exit 1
-#else
-#    LAST_COMMITED_VERSION="`git describe $(git rev-list --tags --max-count=1)`"
-#    if [ "v$VERSION" != "$LAST_COMMITED_VERSION" ]; then
-#	echo creating a tag with the version $VERSION
-#        git tag -a "v$VERSION" -m "Generating setup for v$VERSION"
-#	git push origin --tags 
-#    fi
-#fi
+VERSION=`cat build/linux/release/version`
+if [ ! -f build/linux/release/version ]; then
+    echo "version not found"
+    exit 1
+else
+    LAST_COMMITED_VERSION="`git describe $(git rev-list --tags --max-count=1)`"
+    if [ "v$VERSION" != "$LAST_COMMITED_VERSION" ]; then
+	echo creating a tag with the version $VERSION
+        git tag -a "v$VERSION" -m "Generating setup for v$VERSION"
+	git push origin --tags 
+    fi
+fi
 
 cp resources/linux/run.sh build/linux/release/product
 chmod +x build/linux/release/product/run.sh
