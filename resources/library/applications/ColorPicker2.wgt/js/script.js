4c85e9f56ad0473a5d3f830a724ad0517f2e3144
camille colomb <colomb.c26gmail.com>
2012-05-18 19:56:43 +0200

    ajout de widget

---
+++ resources/library/applications/ColorPicker2.wgt/js/script.js
@@ -0,0 +1,66 @@
+var canvas;
+var ctx;
+
+var images = [ // predefined array of used images
+    'images/colormap.gif',
+    'images/pic2.jpg',
+];
+var iActiveImage = 0;
+
+function toHex(n) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
+
+$(function(){
+
+    // drawing active image
+    var image = new Image();
+    image.onload = function () {
+        ctx.drawImage(image, 0, 0, image.width, image.height); // draw the image on the canvas
+    }
+    image.src = images[iActiveImage];
+
+    // creating canvas object
+    canvas = document.getElementById('panel');
+    ctx = canvas.getContext('2d');
+
+    $('#panel').mousemove(function(e) { // mouse move handler
+        var canvasOffset = $(canvas).offset();
+        var canvasX = Math.floor(e.pageX - canvasOffset.left);
+        var canvasY = Math.floor(e.pageY - canvasOffset.top);
+
+        var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+        var pixel = imageData.data;
+
+        var pixelColor = "rgba("+pixel[0]+", "+pixel[1]+", "+pixel[2]+", "+pixel[3]+")";
+        $('#preview').css('backgroundColor', pixelColor);
+    });
+
+    $('#panel').click(function(e) { // mouse click handler
+        var canvasOffset = $(canvas).offset();
+        var canvasX = Math.floor(e.pageX - canvasOffset.left);
+        var canvasY = Math.floor(e.pageY - canvasOffset.top);
+
+        var imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+        var pixel = imageData.data;
+
+        var dColor = toHex(pixel[0]) + toHex(pixel[1]) + toHex(pixel[2]);
+		
+		var pixelColor = "rgba("+pixel[0]+", "+pixel[1]+", "+pixel[2]+", "+pixel[3]+")";
+		//alert (dColor);		
+		        window.sankore.setTool('pen'); 
+		
+        window.sankore.setPenColor('#' + dColor);
+        sankore.returnStatus("PEN installed", true);
+    }); 
+
+    $('#swImage').click(function(e) { // switching images
+        iActiveImage++;
+        if (iActiveImage >= 10) iActiveImage = 0;
+        image.src = images[iActiveImage];
+    });
+});
