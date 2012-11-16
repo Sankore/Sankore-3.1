3d4bb993f3f597c6c687c7c3845a569966860ec8
Claudio Valerio <claudio@open-sankore.org>
2012-11-16 14:11:50 +0100

    updated all interactivities

---
+++ resources/library/interactivities/Transformation.wgt/Des.wgt/js/help_fr-template.js
@@ -0,0 +1,16 @@
+<h3>Dés</h3>
+<h4>Calcul oral ou jeux divers</h4>
+
+<p>L'interactivité “Dés” vous permet d'afficher des faces de dés de manière aléatoire. </p>
+
+<p>En cliquant sur la flèche ou "Lancer" vous affichez une nouvelle série de résultats. </p>
+
+<p>Les calculs et le raisonnement peuvent être saisis sur le tableau (en dehors de l'interactivité). </p>
+
+<p>Le bouton “Modifier” vous permet :</p>
+<ul>
+<li>de choisir le thème de l’interactivité : tablette, ardoise ou aucun (par défaut tablette), </li>
+<li>de déterminer le nombre de dés que vous voulez utiliser pour votre activité (de 2 à 6). </li>
+</ul>
+
+<p>Le bouton “Afficher” vous permet d’utiliser l’activité.</p>
---
+++ resources/library/interactivities/Transformation.wgt/Tables.wgt/js/help_fr-template.js
@@ -0,0 +1,15 @@
+<h3>Tables</h3>
+<h4>Calcul mental,  propriétés des opérations</h4>
+
+<p>L’interactivité permet de réviser les tables d’addition, de soustraction, de multiplication et de division et de vérifier certaines propriétés.</p>
+
+<p>La flèche sur le tableau indique le sens de lecture.</p>
+
+<p>Le bouton “Recharger” réinitialise l’exercice.</p>
+
+<p>Le bouton "Modifier" vous permet de choisir : </p>
+<ul><li>le thème de l’interactivité : tablette, ardoise ou aucun (par défaut tablette),</li>
+<li>l’opération souhaitée : addition, soustraction, multiplication ou division,</li>
+<li>le nombre de lignes et de colonnes (1 à 12).</li></ul>
+
+<p>Le bouton “Afficher” vous permet d’utiliser l’activité.</p>
